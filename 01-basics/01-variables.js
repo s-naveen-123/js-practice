2bae818caf27500e28a3edb9c4a9843db7e8f96b
s-naveen-123 <140648201+s-naveen-123@users.noreply.github.com>
2025-05-06 14:00:50 +0000

    with variables

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,15 @@
+const accountId = 1
+let accountEmail = "beginner@gmail.com"
+var password = "qweasdzxc"
+//accountId = 3
+console.log(accountId);
+accountEmail = "qwerty@gmail.com"
+/*
+var not be preferred as issue with global and functional scope
+*/
+console.log(accountEmail);
+accountCity = "bandi"
+password = 21235879
+let accountState
+console.table([accountId,accountEmail,password,accountCity,accountState])
+
